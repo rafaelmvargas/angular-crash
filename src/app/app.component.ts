a69b209b8e517023dffb6c898c94bbde5de3dad8
Rafael Vargas <rafael.vargas@outlook.com>
2022-10-01 23:58:13 -0400

    use interpolation via curly braces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'angular-crash';
+  title: string = 'Task Tracker';
 }
