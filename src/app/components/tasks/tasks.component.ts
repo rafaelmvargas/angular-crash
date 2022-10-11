83ae79e57cbebf5795aa942d0b5a1e1354c8dfa8
Rafael Vargas <rafael.vargas@outlook.com>
2022-10-11 04:40:34 +0000

    tasks component data

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
